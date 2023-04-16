f36aec52dcf218d81d4fa25fb6565741d88260e3
GiorgosNik <giorgosnl17@gmail.com>
2023-04-16 17:56:02 +0300

    Server Complete, Sonar

--- salary_calculator_server/salary_calculator_server/settings.py
+++ salary_calculator_server/salary_calculator_server/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-1mpzv_!c+9w@sw0my6u&d_8r3c=c*qj(kje-rsnjx*&su&k8@1"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["dev-salary-calculator.azurewebsites.net"]
 
 
 # Application definition
