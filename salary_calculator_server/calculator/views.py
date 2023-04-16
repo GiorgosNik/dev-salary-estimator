3cd9ce44ddab9dc2d83d583e0e5bd712c01dcf10
GiorgosNik <giorgosnl17@gmail.com>
2023-04-16 11:19:00 +0300

    Receive request with data

--- salary_calculator_server/calculator/views.py
+++ salary_calculator_server/calculator/views.py
@@ -2,43 +2,44 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from tensorflow import keras
 
+model = keras.models.load_model("../model")
+
 
 class UseModel(APIView):
-    def get(self, request):
-        model = keras.models.load_model("../model")
+    def post(self, request):
         input = {
-            "years_experience": 1,
-            "company_size_xf": "11-50",
-            "education_xf": "Bachelor's",
-            "relevant_xf": "Ναι",
-            "personal_projects_xf": "Ναι",
-            "remote_xf": "Και τα δύο",
-            "supervisor_xf": "Ναι",
-            "sex_xf": "Άντρας",
-            "devtype_Backend": 1,
-            "devtype_Desktopapps": 0,
-            "devtype_DevOps": 0,
-            "devtype_AI/ML": 0,
-            "devtype_BI": 0,
-            "devtype_Cybersecurity": 0,
-            "devtype_Embedded": 0,
-            "devtype_Gaming": 0,
-            "devtype_Frontend": 1,
-            "devtype_Mobileapps": 0,
-            "languages_C": 0,
-            "languages_SQL": 0,
-            "languages_PHP": 0,
-            "languages_JavaScript": 1,
-            "languages_Kotlin": 0,
-            "languages_TypeScript": 0,
-            "languages_Python": 1,
-            "languages_Ruby": 0,
-            "languages_Bash": 0,
-            "languages_Go": 0,
-            "languages_Java": 0,
-            "languages_Swift": 0,
-            "languages_Csharp": 0,
-            "languages_Cpp": 0,
+            "years_experience": request.data["years_experience"],
+            "company_size_xf": request.data["company_size"],
+            "education_xf": request.data["education"],
+            "relevant_xf": request.data["relevant"],
+            "personal_projects_xf": request.data["personal_projects"],
+            "remote_xf": request.data["remote"],
+            "supervisor_xf": request.data["supervisor"],
+            "sex_xf": request.data["sex"],
+            "devtype_Backend": request.data["Backend"],
+            "devtype_Desktopapps": request.data["Desktopapps"],
+            "devtype_DevOps": request.data["DevOps"],
+            "devtype_AI/ML": request.data["AI/ML"],
+            "devtype_BI": request.data["BI"],
+            "devtype_Cybersecurity": request.data["Cybersecurity"],
+            "devtype_Embedded": request.data["Embedded"],
+            "devtype_Gaming": request.data["Gaming"],
+            "devtype_Frontend": request.data["Frontend"],
+            "devtype_Mobileapps": request.data["Mobileapps"],
+            "languages_C": request.data["C"],
+            "languages_SQL": request.data["SQL"],
+            "languages_PHP": request.data["PHP"],
+            "languages_JavaScript": request.data["JavaScript"],
+            "languages_Kotlin": request.data["Kotlin"],
+            "languages_TypeScript": request.data["TypeScript"],
+            "languages_Python": request.data["Python"],
+            "languages_Ruby": request.data["Ruby"],
+            "languages_Bash": request.data["Bash"],
+            "languages_Go": request.data["Go"],
+            "languages_Java": request.data["Java"],
+            "languages_Swift": request.data["Swift"],
+            "languages_Csharp": request.data["Csharp"],
+            "languages_Cpp": request.data["Cpp"],
         }
 
         input = {k: [v] for k, v in input.items()}
--- salary_calculator_server/salary_calculator_server/settings.py
+++ salary_calculator_server/salary_calculator_server/settings.py
@@ -40,6 +40,12 @@ INSTALLED_APPS = [
     "rest_framework",
 ]
 
+REST_FRAMEWORK = {
+    "DEFAULT_PARSER_CLASSES": [
+        "rest_framework.parsers.JSONParser",
+    ]
+}
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
